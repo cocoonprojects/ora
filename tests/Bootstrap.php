87724719062252596e6e8692482556bec5e8ddda
Giovanni Di Campli <giovanni.dicampli@carmati.it>
2014-07-23 10:53:35 +0000

    tests configuration fix

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 
 chdir(dirname(__DIR__));
+
+$path = __DIR__ . '/../vendor/zendframework/zendframework/library';
+putenv("ZF2_PATH=".$path);
+
 include __DIR__ . '/../src/init_autoloader.php';
