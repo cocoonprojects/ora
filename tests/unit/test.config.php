bb0bf039205cbec477c1b9632ee97a1613225322
root <root@debian3.carmati>
2014-07-23 12:29:11 +0200

    tests configuration fix

--- tests/unit/test.config.php
+++ tests/unit/test.config.php
@@ -7,7 +7,7 @@ return array(
     'module_listener_options' => array(
         'module_paths' => array(
             __DIR__.'/../../src/module',
-            './vendor',
+            __DIR__.'/../../vendor',
         ),
         'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
