8e628ca4ad7acfe32c70403ecd5db8f653a18b44
Andrea Bandera <dottorbabba@gmail.com>
2014-09-12 09:14:59 +0200

    Doctrine annotations added

--- src/library/Ora/Entity.php
+++ src/library/Ora/Entity.php
@@ -1,15 +1,28 @@
 <?php
 namespace Ora;
 
+use Ora\EventStore\EventStore;
+
 class Entity {
 	
+	/**
+	 * @Id @Column(type="string") 
+	 * @var string
+	 */
 	private $id;
 	
+	/**
+	 * @Column(type="datetime")
+	 * @var DateTime
+	 */
 	private $createdAt;
+	
+	private $es;
 		
-	public function __construct($id, \DateTime $createdAt) {
+	public function __construct($id, \DateTime $createdAt, EventStore $es) {
 		$this->id = $id;
 		$this->createdAt = $createdAt;
+		$this->es = $es;
 	}
 	
 	public function getId() {
@@ -18,5 +31,20 @@ class Entity {
 	
 	public function getCreatedAt() {
 		return $this->createdAt;
+	}
+	
+	public function rebuild($events) {
+		foreach ($events as $e) {
+			$this->apply($e);
+		}
+	}
+	
+	protected function appendToStream(DomainEvent $e) {
+		return $this->es->appendToStream($e);
+	}
+	
+	private function apply(DomainEvent $e) {
+		$method = 'apply'.get_class($event);
+		$this->$method($event);
 	}
 }
