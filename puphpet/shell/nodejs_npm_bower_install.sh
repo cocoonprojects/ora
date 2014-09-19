de6ba02121da42f561e4326f6f9b04f23379138b
Andrea Bandera <dottorbabba@gmail.com>
2014-09-19 12:51:39 +0200

    Merge with doctrine_zf2_integration branch

---
+++ puphpet/shell/nodejs_npm_bower_install.sh
@@ -0,0 +1,13 @@
+#installazione di nodejs
+git clone https://github.com/joyent/node.git
+cd node
+git checkout v0.10.31 #Try checking nodejs.org for what the stable version is
+./configure && make && make install
+cd ..
+
+#installazione di npm
+curl https://www.npmjs.org/install.sh | sh
+
+
+#installazione di bower
+npm install -g bower --allow-root
