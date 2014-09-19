6c972867c1f3950917bb5e0ae4865dc3b0a767e1
Andrea Bandera <dottorbabba@gmail.com>
2014-09-19 13:00:58 +0200

    Merge with doctrine_zf2_integration branch

--- src/module/Application/Module.php
+++ src/module/Application/Module.php
@@ -56,6 +56,7 @@ class Module
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+     				'Ora' => __DIR__ . '/../../library/Ora',              
                 ),
             ),
         );
