4f95c7e0a51555211297a3dc0a62869fa316821d
Roberta D'Amico <roberta.damico@carmati.it>
2014-09-30 13:40:30 +0000

    add doctrine module

--- src/module/Application/Module.php
+++ src/module/Application/Module.php
@@ -44,11 +44,12 @@ class Module
 
 
     public function getAutoloaderConfig()
-    {
+    {    	
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+     				'Ora' => __DIR__ . '/../../library/Ora'            
                 ),
             ),
         );
