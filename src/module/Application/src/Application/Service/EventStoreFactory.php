ec19bbf34cec672d9e3ffea12ff6307ac178132f
Andrea Bandera <dottorbabba@gmail.com>
2014-10-03 18:34:00 +0200

    Namespace fix

--- src/module/Application/src/Application/Service/EventStoreFactory.php
+++ src/module/Application/src/Application/Service/EventStoreFactory.php
@@ -1,14 +1,16 @@
 <?php
-namespace Ora\EventStore;
+namespace Application\Service;
 
 use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Ora\EventStore\DoctrineEventStore;
 
 class EventStoreFactory implements FactoryInterface {
 	
 	private static $instance;
 	
     public function createService(ServiceLocatorInterface $serviceLocator) {
-		$entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
+    	$entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
     	if(is_null(self::$instance)) {
 			self::$instance = new DoctrineEventStore($entityManager);
 		}
