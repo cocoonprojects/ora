82145b9fd39cda68ab7c7dfd1988bada3e1b4ca2
Andrea Bandera <dottorbabba@gmail.com>
2014-10-03 18:37:07 +0200

    All service initialization sequence is performed only if the service isn't already available

--- src/module/Accounting/src/Accounting/Service/CreditsAccountsServiceFactory.php
+++ src/module/Accounting/src/Accounting/Service/CreditsAccountsServiceFactory.php
@@ -2,17 +2,23 @@
 namespace Accounting\Service;
 
 use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Ora\Accounting\EventSourcingCreditsAccountsService;
 
 class CreditsAccountsServiceFactory implements FactoryInterface {
 	
+	/**
+	 * 
+	 * @var EventSourcingCreditsAccountsService
+	 */
 	private static $instance;
 	
 	public function createService(ServiceLocatorInterface $serviceLocator) {
-		$eventStore = $serviceLocator->get('Application\Service\EventStore');
 		if(is_null(self::$instance)) {
+			$eventStore = $serviceLocator->get('Application\Service\EventStore');
 			self::$instance = new EventSourcingCreditsAccountsService($eventStore);
 		}
-		return $instance;
+		return self::$instance;
 	}
 	
 }
