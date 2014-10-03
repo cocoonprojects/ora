b6da5d6b337a916e7f8236db8c4066b8fafa7d32
Andrea Bandera <dottorbabba@gmail.com>
2014-10-03 19:37:07 +0200

    attributes valorization

--- src/library/Ora/Accounting/CreditsAccountEvent.php
+++ src/library/Ora/Accounting/CreditsAccountEvent.php
@@ -18,6 +18,11 @@ class CreditsAccountEvent extends DomainEvent {
 		parent::__construct($firedAt);
 		$this->account = $account;
 		$this->aggregateId = $account->getId();
+		$this->attributes['account']['id'] = $account->getId();
+		$this->attributes['account']['createdAt'] = $account->getCreatedAt();
+		$this->attributes['account']['currency'] = $account->getCurrency();
+		$this->attributes['account']['balance']['value'] = $account->getBalance()->getValue();
+		$this->attributes['account']['balance']['date'] = $account->getBalance()->getDate();
 	}
 
 }
