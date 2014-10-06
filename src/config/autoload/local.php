16eb3b41e445cfbd1ff70f94688ae65e35ca724b
Kuzan <andrea.lupia@gmail.com>
2014-10-06 11:53:17 +0200

    Revert "Added KanbanizeService"
    This reverts commit 15ea0315647df5da9b681e20ceb1d21a55e86a48.

--- src/config/autoload/local.php
+++ src/config/autoload/local.php
@@ -24,9 +24,5 @@ return array(
                 )
             )
         )
-    ),
-    'kanbanize'	=> array(
-    	'apikey' 	=> 'PHhUvBTSY8KdiZZpUb25KNFrMaUxXvVauUkPPRSu',
-    	'url'		=> 'http://kanbanize.com/index.php/api/kanbanize'
     )
 );
