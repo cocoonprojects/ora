0cbd815230008aaf7ccb7ecbbb2742e3b8492ffb
Giannotti Fabio <giannotti.fabio@carmati.it>
2014-10-07 13:53:16 +0000

    Added support to EventStoreFactory

--- src/module/Application/Module.php
+++ src/module/Application/Module.php
@@ -36,11 +36,15 @@ class Module
     } 
     
     // Service Manager Configuration
-    public function getServiceConfig() 
+    public function getServiceConfig()
     {
-        return array();
+        return array(
+            'factories' => array(
+                'Application\Service\LoginService' => 'Application\Service\LoginServiceFactory',
+                'Application\Service\EventStore' => 'Application\Service\EventStoreFactory',
+            )
+        );
     }
-
 
     public function getAutoloaderConfig()
     {    	
--- src/module/TaskManagement/src/TaskManagement/Service/TaskServiceFactory.php
+++ src/module/TaskManagement/src/TaskManagement/Service/TaskServiceFactory.php
@@ -6,28 +6,27 @@ use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 use Ora\TaskManagement\EventSourcingTaskService;
-use Ora\EventStore\DoctrineEventStore;
 use Ora\EntitySerializer;
 
 class TaskServiceFactory implements FactoryInterface 
 {
-    protected $taskService;
-    protected $entitySerializer;
+    /**
+     *
+     * @var EventSourcingTaskService
+     */
+    private static $instance;
     
 	public function createService(ServiceLocatorInterface $serviceLocator) 
 	{
-	    $entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
-	    
-	    if(is_null($this->entitySerializer)) {
-	       $this->entitySerializer = new EntitySerializer($entityManager);
-	    }
-	    
-	    //TODO: richiamare DoctrineEventStore::instance($entityManager) 
-	    // tramite il factory creato da Andrea in Application/Service	    
-	    if(is_null($this->taskService)) {
-	        $this->taskService = new EventSourcingTaskService(DoctrineEventStore::instance($entityManager), $this->entitySerializer);
-	    }
-	    
-	    return $this->taskService;
+	    if(is_null(self::$instance)) {
+            $eventStore = $serviceLocator->get('Application\Service\EventStore');
+
+            $entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
+            $entitySerializer = new EntitySerializer($entityManager);
+            
+            self::$instance = new EventSourcingTaskService($eventStore, $entitySerializer);            
+        }
+
+	    return self::$instance;
 	}
 }
