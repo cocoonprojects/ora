fa97e01cdf4cceafe253fcaf037f1c1ca6824042
Kuzan <andrea.lupia@gmail.com>
2014-10-08 09:37:10 +0200

    Moved tests dir in previous location

--- tests/unit/test.config.php
+++ tests/unit/test.config.php
@@ -6,7 +6,7 @@ return array(
 
     'module_listener_options' => array(
         'module_paths' => array(
-            __DIR__.'/../../module',
+            __DIR__.'/../../src/module',
             __DIR__.'/../../vendor',
         ),
         'config_glob_paths' => array(
