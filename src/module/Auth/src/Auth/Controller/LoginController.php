176710192b7916144d0fed5b21e8d68ff3a50118
Roberta D'Amico <roberta.damico@carmati.it>
2014-10-21 15:38:25 +0000

    User module

--- src/module/Auth/src/Auth/Controller/LoginController.php
+++ src/module/Auth/src/Auth/Controller/LoginController.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace Auth\Controller;
@@ -66,4 +67,92 @@ class LoginController extends AbstractActionController
     
     	return $this->authenticationService;
     }    
+=======
+<?php
+
+namespace Auth\Controller;
+
+use Zend\View\Model\JsonModel;
+use Zend\Session\Container;
+
+use Zend\View\Model\ViewModel;
+use Zend\Mvc\MvcEvent;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class LoginController extends AbstractActionController
+{    
+    
+	private $authenticationService;
+	private $userService;
+			
+    public function loginAction()
+    {    	
+    	$resultAuthentication = array();
+    	
+    	$provider = $this->params('id');
+    	$provider = ucfirst($provider);
+    	
+    	$availableProviderList = $this->getServiceLocator()->get('providerInstanceList');
+    	
+    	if(strlen($this->params('code')) > 10)
+    	{
+    		// errore 500 - scrivi in log code: $this->getRequest()->getQuery('code') 
+    		$this->response->setStatusCode(500);
+    		return;
+    	} 
+
+    	if("" === $provider 
+	    		|| !array_key_exists($provider, $availableProviderList))
+	    {
+	    	$this->response->setStatusCode(503);
+	    	return;		
+	    }	
+	    	   
+	    $instanceProvider = $availableProviderList[$provider];
+		
+	    if($instanceProvider->getToken($this->request))
+	    {	      
+	    	$adapter = $this->getServiceLocator()->get('ZendOAuth2\Auth\Adapter');
+	    	$adapter->setOAuth2Client($instanceProvider); 
+	
+	    	$authenticationService = $this->getAuthenticationService();
+	    	$authenticate = $authenticationService->authenticate($adapter); // return Zend\Authentication\Result
+
+	    	if($authenticate->isValid())
+	    	{
+	    		$userService = $this->getUserService();	 
+				$user = $userService->createNewUser($authenticationService->getIdentity());
+	    		
+		    	$view = new ViewModel(array(
+		    			'authenticate' => $authenticate
+		    	));
+	    	
+	    		return $view;
+	    	}	    				
+	    }
+    }  
+
+    protected function getAuthenticationService()
+    {
+    	if (!isset($this->authenticationService))
+    	{
+    		$serviceLocator = $this->getServiceLocator();
+    		$this->authenticationService = $serviceLocator->get('Auth\Service\AuthenticationService');
+    	}
+    
+    	return $this->authenticationService;
+    }  
+
+    protected function getUserService()
+    {
+    	if (!isset($this->userService))
+    	{
+    		$serviceLocator = $this->getServiceLocator();
+    		$this->userService = $serviceLocator->get('User\Service\UserService');
+    	}
+    	
+    	return $this->userService;    	
+    }
+>>>>>>> User module
 }
--- src/module/Auth/src/Auth/View/Helper/AuthenticationAction.php
+++ src/module/Auth/src/Auth/View/Helper/AuthenticationAction.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 namespace Auth\View\Helper;
 
@@ -38,4 +39,46 @@ class AuthenticationAction extends AbstractHelper implements ServiceLocatorAware
 	{
 		return $this->serviceLocator;
 	}	
+=======
+<?php
+namespace Auth\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class AuthenticationAction extends AbstractHelper implements ServiceLocatorAwareInterface
+{	
+	public function __invoke()
+	{				
+		$serviceLocator = $this->getServiceLocator();		
+		$authenticationService = $serviceLocator->get('Auth\Service\AuthenticationService');
+		
+		if($authenticationService->hasIdentity())
+		{
+			$user = $authenticationService->getIdentity();
+			
+			$output = "<li><img src='{$user['picture']}' alt='picture' class='img-circle' style='max-width: 40px'></li>";
+			$output .= "<li><a>{$user['firstname']} {$user['lastname']}</a></li>";
+			$output .= "<li><a href='/auth/logout'>Logout</a></li>";
+			
+			return $output;		
+		}
+
+		$output = "<li><a id='login-auth'>Login</a></li>";
+
+		return $output;
+	}	
+	
+	public function setServiceLocator(ServiceLocatorInterface $helperPluginManager)
+	{
+		$this->serviceLocator = $helperPluginManager->getServiceLocator();
+		return $this;
+	}
+
+	public function getServiceLocator()
+	{
+		return $this->serviceLocator;
+	}	
+>>>>>>> User module
 }
