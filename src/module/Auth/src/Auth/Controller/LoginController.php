42159be619033f035a4998479e23130c65fa5ec0
Roberta D'Amico <roberta.damico@carmati.it>
2014-10-23 12:04:58 +0000

    login

--- src/module/Auth/src/Auth/Controller/LoginController.php
+++ src/module/Auth/src/Auth/Controller/LoginController.php
@@ -190,7 +190,7 @@ class LoginController extends AbstractActionController
 	    
 	    $authenticationService = $this->getAuthenticationService();
 	    $authenticate = $authenticationService->authenticate($adapter); // return Zend\Authentication\Result
-	    	    
+
 	    $view->setVariable('authenticate', $authenticate);
 	    	    	    	
 	    return $view;	
