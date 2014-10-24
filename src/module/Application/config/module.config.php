e42ecc5d55488bf2cb6c6a96981f63f992c9208b
Giannotti Fabio <giannotti.fabio@carmati.it>
2014-10-24 10:48:38 +0000

    Last fix to Doctrine Proxyes directory. I Hope

--- src/module/Application/config/module.config.php
+++ src/module/Application/config/module.config.php
@@ -102,7 +102,7 @@ return array(
     	'configuration' => array(
         	'orm_default' => array(
         		'generate_proxies'  => true,
-        		'proxy_dir'         => '/vagrant/src/data/DoctrineORMModule/Proxies'        		
+        		'proxy_dir'         => __DIR__ . '/../../../data/DoctrineORMModule/Proxies/'        		
         	)
         ),
     
