3084ce462985b148336064b73f153f9849809be8
Andrea Bandera <dottorbabba@gmail.com>
2014-11-03 14:44:25 +0100

    Fix bug on initialization of controllers

--- src/module/TaskManagement/Module.php
+++ src/module/TaskManagement/Module.php
@@ -4,10 +4,24 @@ namespace TaskManagement;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Stdlib\InitializableInterface;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {    
-    public function getConfig()
+	public function onBootstrap(MvcEvent $e)
+	{
+        $sm = $e->getApplication()->getServiceManager();
+
+        $controllers = $sm->get('ControllerLoader');            
+        $controllers->addInitializer(function($controller, $cl) {
+            if ($controller instanceof InitializableInterface) {
+                $controller->init();
+            }
+        }, false); // false tells the loader to run this initializer after all others
+    }
+	
+	public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
