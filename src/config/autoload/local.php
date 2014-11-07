04572626b748d0eabd143353711d60fcc7b1be6a
Kuzan <andrea.lupia@gmail.com>
2014-11-07 13:09:11 +0100

    Removed apikeys

--- src/config/autoload/local.php
+++ src/config/autoload/local.php
@@ -1,8 +1,5 @@
 <?php
 
 return array(
-	'kanbanize'	=> array(
-				'apikey' 	=> 'PHhUvBTSY8KdiZZpUb25KNFrMaUxXvVauUkPPRSu',
-				'url'		=> 'http://kanbanize.com/index.php/api/kanbanize'
-		)
+		
 );
