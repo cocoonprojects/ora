c640c5e33be1ab71217488778e3aab97dcaf045b
mario7102 <mario.tilli@carmati.it>
2014-12-05 09:28:51 +0000

    fixed test

--- tests/functional/bootstrap/TaskContext.php
+++ tests/functional/bootstrap/TaskContext.php
@@ -17,36 +17,42 @@ class TaskContext extends RestContext
     {
        	$tasks = json_decode($this->getResponse()->getBody(true))->tasks;
     	
+       	$existingCounter = 0;
+       	
        	if(!empty($tasks)){
 	       	foreach($tasks as $task){
+	       		
+	       		$exists = true;
+	       		
 	       		if (isset($task->$searchArray)) {
 	       		
 		       		$data = $task->$searchArray;
 					if(!empty($data)){
-						
-						foreach($data as $element){         
-			            	 
-			                if (! isset($element->$searchParameter)) {
-			                    throw new Exception("Property {$searchParameter} is not set in elements of {$data} array!\n");
-			                }else{
-			                	if($parameterValue == 'not null'){
-			                		if( empty($element->$searchParameter)){
-				                		throw new Exception("Property {$searchParameter} is empty!\n");	
-			                		} 	
-			                	}else{
-			                		if($element->$searchParameter !== $parameterValue){
-			                			throw new Exception("Property {$searchParameter} value {} doesn't match expected value {$parameterValue}!\n");
-			                		}
-			                	}			                	
-			                }
+												
+						foreach($data as $element){
+			                if($parameterValue == 'not null'){
+		                		if( !empty($element->$searchParameter)){
+			                		//throw new Exception("Property {$searchParameter} is empty!\n");
+			                		$existingCounter++;
+		                		}		                		
+		                	}else{
+		                		if($element->$searchParameter == $parameterValue){
+		                			//throw new Exception("Property {$searchParameter} value {$element->$searchParameter} doesn't match expected value {$parameterValue}!\n");
+		                			$existingCounter++;
+		                		}
+		                	}
 			            }	
-					}		       		
+					}
 		            
-		        } else {
+		        }else {
 		            throw new Exception(
 		                    "{$searchArray} not found into tasks!");
 		        }		
 	       	}
+	       		       	
+       		if($existingCounter == 0){
+				throw new Exception("Property {$searchParameter} value doesn't match expected value {$parameterValue}!\n");
+			}
 	       	
        	}else{
        	    throw new Exception(
