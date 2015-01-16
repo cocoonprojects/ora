b5f2e9476b9aa97f07c39e3237f24219a68c40dd
Andrea Bandera <dottorbabba@gmail.com>
2015-01-16 14:49:28 +0100

    Fix

---
+++ src/public/js/app.js
@@ -0,0 +1,19 @@
+var oraApp = angular.module('oraApp', [
+   'ngRoute',
+   'taskManagementControllers'
+]);
+oraApp.config(['$routeProvider',
+   function($routeProvider) {
+       $routeProvider.
+           when('/tasks', {
+        	   templateUrl: 'partials/task-list.html',
+               controller: 'TaskListCtrl'
+           }).
+           when('/tasks/:taskId', {
+               templateUrl: 'partials/task-detail.html',
+               controller: 'TaskDetailCtrl'
+           }).
+           otherwise({
+               redirectTo: '/tasks'
+           });
+    }]);
