24140a727b066cc8cad34ffd8794f97c233ec82a
Andrea Bandera <dottorbabba@gmail.com>
2015-01-27 15:18:47 +0100

    Add angular services

--- src/public/js/controllers.js
+++ src/public/js/controllers.js
@@ -1,17 +1,15 @@
 var taskManagementControllers = angular.module('taskManagementControllers', []);
 
-taskManagementControllers.controller('TaskListCtrl', ['$scope', '$http',
-    function ($scope, $http) {
-		$http.get('task-management/tasks').success(function(data) {
-			$scope.tasks = data.tasks;
-		});
+taskManagementControllers.controller('TaskListCtrl', ['$scope', 'Task',
+    function ($scope, Task) {
+		$scope.tasks = Task.query();
 	}
 ]);
 
-taskManagementControllers.controller('TaskDetailCtrl', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
-		$http.get('task-management/tasks/' + $routeParams.taskId).success(function(data) {
-			$scope.task = data;
+taskManagementControllers.controller('TaskDetailCtrl', ['$scope', '$routeParams', 'Task',
+    function($scope, $routeParams, Task) {
+		$scope.task = Task.get({taskId : $routeParams.taskId }, function(task) {
+			
 		});
 	}
 ]);
