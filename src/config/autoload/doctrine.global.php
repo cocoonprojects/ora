c6fb77d84d28bb6e7f6c6130919751b32b3cc14d
Andrea Bandera <dottorbabba@gmail.com>
2015-03-02 12:06:06 +0100

    Debug 7

--- src/config/autoload/doctrine.global.php
+++ src/config/autoload/doctrine.global.php
@@ -4,11 +4,11 @@ return array(
 		'connection' => array(
 			'orm_default' => array(
 				'params' => array(
-					'host'			=> getenv('DB_HOSTNAME'),
-					'port'			=> getenv('DB_PORT'),
-					'user'			=> getenv('DB_USERNAME'),
-					'password'		=> getenv('DB_PASSWORD'),
-					'dbname'		=> getenv('DB_NAME'),
+					'host'			=> '127.0.0.1',
+					'port'			=> '3306',
+					'user'			=> 'travis',
+					'password'		=> '',
+					'dbname'		=> 'oraproject_test',
 					'driverOptions' => array(
 						\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 					),
