609d1ee30580e678f6281e5814f32c92515f30f7
Andrea Bandera <dottorbabba@gmail.com>
2015-03-02 18:40:49 +0100

    Add acceptance config files

---
+++ src/config/autoload/doctrine.acceptance.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+	'doctrine' => array(
+		'connection' => array(
+			'orm_default' => array(
+				'params' => array(
+					'host'			=> '127.0.0.1',
+					'port'			=> 3306,
+					'user'			=> 'travis',
+					'password'		=> '',
+					'dbname'		=> 'oraproject_test',
+					'driverOptions' => array(
+						\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+					),
+				)
+			)
+		)
+	)
+);
