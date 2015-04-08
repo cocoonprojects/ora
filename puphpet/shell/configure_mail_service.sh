d606797f62b2178b63b701713ebcb53979deb219
mario7102 <mario.tilli@carmati.it>
2015-04-08 07:39:30 +0000

    adding hosts for mail service

--- puphpet/shell/configure_mail_service.sh
+++ puphpet/shell/configure_mail_service.sh
@@ -12,7 +12,7 @@ else
 	echo '#' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
 	echo '#' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
 	echo '# Google nameservers' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
-	echo 'nameserver 8.8.4.4' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+	echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
 	echo '#' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
 	echo '#' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
 	
@@ -20,14 +20,27 @@ else
 
 fi
 
+
+#con il nuovo nameserver dobbiamo modificare il file /etc/hosts sia per velocizzare l'invio delle email sia per rendere raggiungibile l'host 'oraprojecttest' durante l'esecuzione dei test di accettazione
+
 HOSTNAME=$(hostname)
 if grep -Fxq "127.0.0.1 $HOSTNAME localhost.localdomain" /etc/hosts
 then
-	echo "...hostname gia' configurato..."
+	echo "...hostname $HOSTNAME gia' configurato..."
 else
 	echo "127.0.0.1 $HOSTNAME localhost.localdomain" | sudo tee -a /etc/hosts
 	
-	echo "...hostname configurato correttamente..."
+	echo "...hostname $HOSTNAME configurato correttamente..."
+fi
+
+HOSTNAME_TEST='oraprojecttest'
+if grep -Fxq "192.168.56.111 $HOSTNAME_TEST localhost.localdomain" /etc/hosts
+then
+	echo "...hostname $HOSTNAME_TEST gia' configurato..."
+else
+	echo "192.168.56.111 $HOSTNAME_TEST localhost.localdomain" | sudo tee -a /etc/hosts
+	
+	echo "...hostname $HOSTNAME_TEST configurato correttamente..."
 fi
 
 echo "...FATTO!"
