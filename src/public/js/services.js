48c87a3e7355f30ba960698d9cd6edc68de3a509
Andrea Bandera <dottorbabba@gmail.com>
2015-04-16 14:59:12 +0000

    Remove angularjs

--- src/public/js/services.js
+++
@@ -1,8 +0,0 @@
-var taskManagementServices = angular.module('taskManagementServices', ['ngResource']);
-
-taskManagementServices.factory('Task', ['$resource',
-    function($resource){
-		return $resource('task-management/:taskId.json', {}, {
-			query: {method:'GET', params:{taskId:'tasks'}, isArray:true}
-		});
-	}]);
