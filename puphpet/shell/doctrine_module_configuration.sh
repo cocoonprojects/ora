74bb2f98f0b359e213f3e5f6349e49a0783d2630
Andrea Bandera <dottorbabba@gmail.com>
2015-04-23 17:36:46 +0000

    Remove VM after rebase

--- puphpet/shell/doctrine_module_configuration.sh
+++
@@ -1,9 +0,0 @@
-echo "INIZIO CONFIGURAZIONE DEL MODULO DI DOCTRINE PER ZF2"
-
-if [ -f /vagrant/config/application.config.php ]; then
-        echo "link simbolico gia' presente."
-    else
-	mkdir /vagrant/config
-	ln -s /vagrant/src/config/application.config.php /vagrant/config/
-	echo "....fatto!"
-fi
