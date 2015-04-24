7ac2dd51af14e6e428b253d95d2f90a44c98f736
Andrea Bandera <dottorbabba@gmail.com>
2015-04-24 13:13:34 +0000

    Moves config, module and public at top level directory. Removes src

---
+++ config/application.config.php
@@ -0,0 +1,38 @@
+<?php
+
+$env = getenv('APPLICATION_ENV') ? : "local";
+
+return array(
+	// This should be an array of module namespaces used in the application.
+	'modules' => array(
+		'DoctrineModule',
+		'DoctrineORMModule',
+		'ProophEventStoreModule',
+		'ZendOAuth2',
+		'AssetManager',
+		'Accounting',
+		'TaskManagement',
+		'Application',
+		'BjyAuthorize',
+		'Kanbanize'
+	),
+
+	// These are various options for the listeners attached to the ModuleManager
+	'module_listener_options' => array(
+		// This should be an array of paths in which modules reside.
+		// If a string key is provided, the listener will consider that a module
+		// namespace, the value of that key the specific path to that module's
+		// Module class.
+		'module_paths' => array(
+			__DIR__.'/../module',
+			__DIR__.'/../vendor',
+		),
+
+		// An array of paths from which to glob configuration files after
+		// modules are loaded. These effectively override configuration
+		// provided by modules themselves. Paths may use GLOB_BRACE notation.
+		'config_glob_paths' => array(
+			__DIR__.'/autoload/{,*.}{global,'.$env.',local}.php',
+		),
+	),		
+);
--- tests/integration/Bootstrap.php
+++ tests/integration/Bootstrap.php
@@ -12,10 +12,10 @@ use Doctrine\ORM\Tools\SchemaTool;
 error_reporting(E_ALL | E_STRICT);
 chdir(dirname(__DIR__));
 
-$path = __DIR__ . '/../../src/vendor/zendframework/zendframework/library';
+$path = __DIR__ . '/../../vendor/zendframework/zendframework/library';
 putenv("ZF2_PATH=".$path);
 
-include __DIR__ . '/../../src/init_autoloader.php';
+include __DIR__ . '/../../init_autoloader.php';
 
 /**
  * Test bootstrap, for setting up autoloading
@@ -28,9 +28,9 @@ class Bootstrap
 
 	public static function init($config)
 	{
-		echo shell_exec(__DIR__ . '/../../src/vendor/bin/doctrine-module orm:schema-tool:drop --force');
-		echo shell_exec(__DIR__ . '/../../src/vendor/bin/doctrine-module orm:schema-tool:create');
-		echo shell_exec(__DIR__ . '/../../src/vendor/bin/doctrine-module dbal:import ' . __DIR__ . '/../sql/init.sql');
+		echo shell_exec(__DIR__ . '/../../vendor/bin/doctrine-module orm:schema-tool:drop --force');
+		echo shell_exec(__DIR__ . '/../../vendor/bin/doctrine-module orm:schema-tool:create');
+		echo shell_exec(__DIR__ . '/../../vendor/bin/doctrine-module dbal:import ' . __DIR__ . '/../sql/init.sql');
 		
 		self::$zendApp = Application::init($config);
 		self::$serviceManager = self::$zendApp->getServiceManager();
@@ -48,4 +48,4 @@ class Bootstrap
 	}
 }
 
-Bootstrap::init(include __DIR__.'/../../src/config/application.config.php');
+Bootstrap::init(include __DIR__.'/../../config/application.config.php');
