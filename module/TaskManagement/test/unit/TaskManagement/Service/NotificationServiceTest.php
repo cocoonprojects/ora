2879e1e0a7c0392ce8b77cb3f05c9dd1435b6afa
Mario Tilli <mario.tilli@carmati.it>
2015-06-05 07:57:40 +0000

    some refactoring

--- module/TaskManagement/test/unit/TaskManagement/Service/NotificationServiceTest.php
+++ module/TaskManagement/test/unit/TaskManagement/Service/NotificationServiceTest.php
@@ -5,6 +5,9 @@ namespace TaskManagement\Service;
 use Zend\View\Renderer\PhpRenderer;
 use Zend\View\Model\ViewModel;
 use Zend\View\Resolver\TemplateMapResolver;
+use Application\Entity\User;
+use TaskManagement\Entity\Task;
+use TaskManagement\Entity\TaskMember;
 
 
 class NotificationServiceTest extends \PHPUnit_Framework_TestCase {
@@ -18,30 +21,34 @@ class NotificationServiceTest extends \PHPUnit_Framework_TestCase {
 	 * @var NotificationService
 	 */
 	private $notificationService;
+	/**
+	 * @var Application\Entity\User
+	 */
+	private $taskMember;
+	/**
+	 * @var TaskManagement\Entity\Task
+	 */
+	private $task;
 	
 	protected function setUp() {
 		
-		$emailTemplates = array('TaskManagement\NotifyMemebersForShareAssignment' => __DIR__.'/../../../../view/task-management/email_templates/reminder-assignment-shares.phtml');
+		$emailTemplates = array('TaskManagement\RemindMembersForAssignmentOfShares' => __DIR__.'/../../../../view/task-management/email_templates/reminder-assignment-shares.phtml');
 		
 		$this->notificationService = new NotificationService($emailTemplates);
 		$this->mailcatcher = new \Guzzle\Http\Client('http://127.0.0.1:1080');
-		$this->cleanEmailMessages();
-		
+		$this->cleanEmailMessages();		
+		$this->setupTaskWithMember();
+		$_SERVER['SERVER_NAME'] = 'example.com';
+	}
+	
+	protected function tearDown(){
+		unset($_SERVER['SERVER_NAME']);
 	}
 	
 	
-	
-	public function testSendEmailNotificationForShareAssignment(){
+	public function testSendEmailNotificationForAssignmentOfShares(){
 		
-		$params = array(
-				'name' => 'Dorian Gray',
-				'taskSubject' => 'new book',
-				'taskId' => '11111ab-1111-1111-1111-11111111c500',
-				'emailAddress' => 'doriangray@email.com',
-				'url' => 'http://www.example.com'
-		);
-
-		$this->notificationService->sendEmailNotificationForShareAssignment($params);
+		$this->notificationService->sendEmailNotificationForAssignmentOfShares($this->task, $this->taskMember);
 		
 		$emails = $this->getEmailMessages();
 		
@@ -49,6 +56,7 @@ class NotificationServiceTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals(1, count($emails));
 		$this->assertEmailSubjectEquals('O.R.A. - your contribution is required!', $emails[0]);
 		$this->assertEmailHtmlContains('new book', $emails[0]);
+		$this->assertEmailHtmlContains('http://example.com/task-management#11111ab-1111-1111-1111-11111111c500', $emails[0]);
 		$this->assertNotEmpty($emails[0]->recipients);
 		$this->assertEquals($emails[0]->recipients[0], '<doriangray@email.com>');
 		
@@ -67,7 +75,7 @@ class NotificationServiceTest extends \PHPUnit_Framework_TestCase {
         return json_decode($jsonResponse->getBody());
     }
     
-    public function getLastEmailMessage()
+    protected function getLastEmailMessage()
     {
     	$messages = $this->getEmailMessages();
     	if (empty($messages)) {
@@ -77,15 +85,28 @@ class NotificationServiceTest extends \PHPUnit_Framework_TestCase {
     	return reset($messages);
     }
     
-    public function assertEmailSubjectEquals($expected, $email, $description = '')
+    protected function assertEmailSubjectEquals($expected, $email, $description = '')
     {
     	$this->assertContains($expected, $email->subject, $description);
     }
     
-    public function assertEmailHtmlContains($needle, $email, $description = '')
+    protected function assertEmailHtmlContains($needle, $email, $description = '')
     {
     	$response = $this->mailcatcher->get("/messages/{$email->id}.html")->send();
     	$this->assertContains($needle, (string)$response->getBody(), $description);
+    }
+    
+    protected function setupTaskWithMember(){
+    	
+    	$this->taskMember = User::create();
+    	$this->taskMember->setFirstname('Gray');
+    	$this->taskMember->setLastname('Dorian');
+    	$this->taskMember->setEmail('doriangray@email.com');
+    	
+    	$this->task = new Task('11111ab-1111-1111-1111-11111111c500');
+    	$this->task->setSubject('new book');
+    	$this->task->addMember($this->taskMember, TaskMember::ROLE_MEMBER, $this->taskMember, new \DateTime());
+    	
     }
     
 }
