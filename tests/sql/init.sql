c0684e193c9fe808dcdca7f96171bb953bd074dc
Mario Tilli <mario.tilli@carmati.it>
2015-09-25 14:06:03 +0000

    some refactoring

--- tests/sql/init.sql
+++ tests/sql/init.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `event_stream` (
 DELETE FROM task_members;
 DELETE FROM tasks;
 DELETE FROM streams;
-DELETE FROM transactions;
+DELETE FROM account_transactions;
 DELETE FROM accounts;
 DELETE FROM organizations;
 DELETE FROM event_stream;
