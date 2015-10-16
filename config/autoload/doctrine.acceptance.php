431f7ea7cf70ecc196ef33b29a174600ca0730f5
AbdonSerianni <serianniabdon@gmail.com>
2015-10-16 10:53:11 +0200

    Fix Auto Proxies Generation in Staging Environment

---
+++ config/autoload/doctrine.acceptance.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+		'doctrine' => array (
+				'configuration' => array(
+						'orm_default' => array(
+								'generate_proxies'	=> true,
+								'proxy_dir'			=> __DIR__ . '/../../../data/DoctrineORMModule/Proxies/'
+						)
+				)
+		)
+);
---
+++ config/autoload/doctrine.staging.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+		'doctrine' => array (
+				'configuration' => array(
+						'orm_default' => array(
+								'generate_proxies'	=> false,
+								'proxy_dir'			=> __DIR__ . '/../../../data/DoctrineORMModule/Proxies/'
+						)
+				)
+		)
+);
