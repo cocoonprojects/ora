0d5e67850bd8cb359954f292228fa5ba0f8ff7cb
Mario Tilli <mario.tilli@carmati.it>
2016-02-16 08:49:36 +0000

    introducing explicit voting on a work item idea

--- module/TaskManagement/src/TaskManagement/Controller/ApprovalsController.php
+++ module/TaskManagement/src/TaskManagement/Controller/ApprovalsController.php
@@ -25,10 +25,6 @@ class ApprovalsController extends HATEOASRestfulController {
 	 * @var TaskService
 	 */
 	protected $taskService;
-	/**
-	 * @var \DateInterval
-	 */
-	protected $timeboxForItemIdeaVoting;
 	
 	public function __construct(TaskService $taskService){
 		$this->taskService = $taskService;
@@ -102,36 +98,6 @@ class ApprovalsController extends HATEOASRestfulController {
 		return $this->response;
 	}
 	
-	public function getList(){
-		
-		if(is_null($this->identity())) {
-			$this->response->setStatusCode(401);
-			return $this->response;
-		}
-		switch ($this->params('type')) {
-			case "idea-items":
-				if(!$this->isAllowed($this->identity(), NULL, 'TaskManagement.Approval.idea-items')){
-					$this->response->setStatusCode(403);
-					return $this->response;
-				}
-				$itemIdeas = $this->taskService->findItemsBefore($this->timeboxForItemIdeaVoting, TaskInterface::STATUS_IDEA);
-				foreach ($itemIdeas as $idea){
-					
-				}
-				var_dump($approvalsToClose);
-				break;
-			default:
-				$this->response->setStatusCode(404);
-		}
-		return $this->response;
-	}
-	
-	public function setTimeboxForItemIdeaVoting(\DateInterval $interval){
-		$this->timeboxForItemIdeaVoting = $interval;
-	}
-	public function getTimeboxForItemIdeaVoting(){
-		return $this->timeboxForItemIdeaVoting;
-	}
 	public function getTaskService(){
 		return $this->taskService;
 	}
@@ -141,8 +107,5 @@ class ApprovalsController extends HATEOASRestfulController {
 	
 	protected function getResourceOptions(){
 		return self::$resourceOptions;
-	}
-	protected static function getDefaultIntervalForItemIdeaVoting(){
-		return new \DateInterval('P7D');
 	}
 }
